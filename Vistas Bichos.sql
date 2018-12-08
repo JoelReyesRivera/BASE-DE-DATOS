7212ffd4fbeb7de0cd938a8f89e587c1ddeb8ec1
Cristina Beltrán <17170564@itculiacan.edu.mx>
2018-12-08 12:28:54 -0700

    Agregacion de 3 vistas

---
+++ Vistas Bichos.sql
@@ -0,0 +1,53 @@
+USE BICHOS
+GO
+
+--VISTA USUARIO
+create view Usuarios 
+as SELECT  [Nombre(s)] +' '+Apellido as [Nombre], UserName FROM USUARIO
+
+GO
+SELECT * FROM Usuarios
+GO
+
+--VISTA USUARIO Y BICHOS QUE LE PERTENECEN
+create view UsuarioBichos
+as SELECT u.UserName, b.Nombre as [Bicho], e.Nombre as [Especie], t.Descripción [Tipo] FROM USUARIO u 
+INNER JOIN BICHO b ON u.ID=b.Propietario
+INNER JOIN ESPECIE e ON b.Especie=e.ID
+INNER JOIN TIPOBICHO t ON t.ID=e.Tipo
+
+GO
+SELECT * FROM UsuarioBichos
+GO
+
+--VISTA BICHO ATAQUES
+create view BichoAtaques
+as SELECT  b.Nombre as [Bicho],e.Nombre as [Especie], t.Descripción [Tipo], a.Descripción as [Ataque], a.Daño [Daño] FROM [BICHO-ATAQUE] ba 
+INNER JOIN BICHO b ON ba.[ID-Bicho] = b.ID
+INNER JOIN ATAQUE a ON ba.[ID-Ataque]=a.ID
+INNER JOIN ESPECIE e ON b.Especie=e.ID
+INNER JOIN TIPOBICHO t ON t.ID=e.Tipo
+
+GO
+SELECT * FROM BichoAtaques
+GO
+
+--VISTA EVOLUCION
+create view Evoluciones
+as 
+
+GO
+SELECT * FROM BichoAtaques
+GO
+
+--INFORMACION ESPECIE
+create view EspecieInfo
+as SELECT * FROM ESPECIE e 
+INNER JOIN TIPOBICHO t ON e.Tipo=t.ID
+INNER JOIN [TIPO-DEBILIDAD] d ON d.[Id-Tipo]=t.ID
+INNER JOIN [TIPO-FORTALEZA] f ON f.[Id-Tipo]=t.ID
+
+
+GO
+SELECT * FROM EspecieInfo
+GO
